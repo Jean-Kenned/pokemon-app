d1c5d02e50947ffba3fc195e979a5b8763ef02e1
Jean Kenned <jeankennedteodoro@gmail.com>
2022-01-12 22:27:22 -0300

    feat: create home text title component

---
+++ src/presentation/components/atoms/HomeTextTitle/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Title} from './styles';
+
+const HomeTextTitle: React.FC = () => {
+  return <Title>Qual pokémon você escolheria?</Title>;
+};
+
+export default HomeTextTitle;
---
+++ src/presentation/components/atoms/HomeTextTitle/styles.ts
@@ -0,0 +1,9 @@
+import {Colors} from '@/presentation/helpers';
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-family: Inter-Regular;
+  color: ${Colors.PRIMARY_TEXT_COLOR};
+  font-weight: 800;
+`;
