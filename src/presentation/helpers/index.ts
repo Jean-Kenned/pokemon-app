7637b10b662b463850d8cf59e0f1bac296509d73
Jean Kenned <jeankennedteodoro@gmail.com>
2022-01-14 15:26:11 -0300

    refactor: export helpers in the index file

--- src/presentation/helpers/index.ts
+++ src/presentation/helpers/index.ts
@@ -1,3 +1,6 @@
 export {default as Fonts} from './fonts';
 export {default as Spacing} from './spacing';
 export {default as Colors} from './colors';
+export {default as getHexFromColorName} from './getHexFromColorName';
+export {default as getFormattedIdNumber} from './getFormattedIdNumber';
+export {default as getNormalizedColorName} from './getNormalizedColorName';
