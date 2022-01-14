b79afb94a50919b720d81873988df4566e00fdc7
Jean Kenned <jeankennedteodoro@gmail.com>
2022-01-14 15:21:05 -0300

    refactor: export components in the index file

--- src/presentation/components/atoms/index.ts
+++ src/presentation/components/atoms/index.ts
@@ -2,3 +2,7 @@ export {default as AvatarImage} from './AvatarImage';
 export {default as HomeTextTitle} from './HomeTextTitle';
 export {default as Logo} from './Logo';
 export {default as IndicatorLoading} from './IndicatorLoading';
+export {default as CardPokemonImage} from './CardPokemonImage';
+export {default as CardPokemonName} from './CardPokemonName';
+export {default as CardPokemonNumber} from './CardPokemonNumber';
+export {default as TypeLabel} from './TypeLabel';
--- src/presentation/components/molecules/index.ts
+++ src/presentation/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export {default as Avatar} from './Avatar';
 export {default as WelcomeText} from './WelcomeText';
 export {default as HomeTitleHeader} from './HomeTitleHeader';
+export {default as TypeLabelsContainer} from './TypeLabelsContainer';
