3475e14a5949dddb66c2e8037ca0ee8a432589ec
Jean Kenned <jeankennedteodoro@gmail.com>
2022-01-16 10:47:42 -0300

    feat: create pokemon page route

--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Home} from '@/presentation/pages';
+import {Home, Pokemon} from '@/presentation/pages';
 import {StatusBar} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootStackParamList} from '@/main/routes/routes.types';
+import {Colors} from '@/presentation/helpers';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
 const Routes: React.FC = () => {
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor="#38383860" />
+      <StatusBar backgroundColor={Colors.HEADER_BG_COLOR} />
       <RootStack.Navigator
         initialRouteName="Home"
         screenOptions={{headerShown: false}}>
         <RootStack.Screen name="Home" component={Home} />
+        <RootStack.Screen name="Pokemon" component={Pokemon} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
--- src/main/routes/routes.types.ts
+++ src/main/routes/routes.types.ts
@@ -1,7 +1,10 @@
+import {PokemonModel} from '@/domain/models';
 import {StackScreenProps} from '@react-navigation/stack';
 
 export type RootStackParamList = {
   Home: undefined;
+  Pokemon: PokemonModel;
 };
 
 export type HomeProps = StackScreenProps<RootStackParamList, 'Home'>;
+export type PokemonProps = StackScreenProps<RootStackParamList, 'Pokemon'>;
