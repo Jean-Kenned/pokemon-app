80c7f021be8f260bcfbda5b43789346fbed59636
Jean Kenned <jeankennedteodoro@gmail.com>
2022-01-17 10:31:29 -0300

    feat: show main characteristics in pokemon page

--- src/presentation/pages/Pokemon/index.tsx
+++ src/presentation/pages/Pokemon/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {PokemonProps} from '@/main/routes/routes.types';
 import {Container, Body, ImageWrapper, Image, Content} from './styles';
-import {PokemonPageHeader} from '@/presentation/components/organisms';
-import {Text} from 'react-native';
+import {
+  PokemonPageHeader,
+  PokemonMainCharacteristics,
+} from '@/presentation/components/organisms';
 
 const Pokemon: React.FC<PokemonProps> = ({route}: PokemonProps) => {
   const {params: pokemon} = route;
@@ -15,7 +17,7 @@ const Pokemon: React.FC<PokemonProps> = ({route}: PokemonProps) => {
           <Image source={{uri: pokemon.image}} />
         </ImageWrapper>
         <Content>
-          <Text style={{color: 'black'}}>Pokemon description </Text>
+          <PokemonMainCharacteristics pokemon={pokemon} />
         </Content>
       </Body>
     </Container>
