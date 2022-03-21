b28b415bb050ac8e13d1ddf3a2321ac9e3ef1199
Jean Kenned <jeankennedteodoro@gmail.com>
2022-03-21 08:49:02 -0300

    refactor: apply linter changes

--- __tests__/helpers/getFormattedIdNumber.spec.ts
+++ __tests__/helpers/getFormattedIdNumber.spec.ts
@@ -1,9 +1,9 @@
-import { getFormattedIdNumber } from '@/presentation/helpers'
+import { getFormattedIdNumber } from '@/presentation/helpers';
 
 describe('getFormattedIdNumber helper function', () => {
     it('should return the correct id number formatted ', () => {
         expect(getFormattedIdNumber(9)).toBe('#009');
         expect(getFormattedIdNumber(50)).toBe('#050');
         expect(getFormattedIdNumber(240)).toBe('#240');
-    })
-})
+    });
+});
