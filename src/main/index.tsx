26d38b53a5fdcafffc66872c1010c1f0c1acb4fd
Jean Kenned <jeankennedteodoro@gmail.com>
2022-03-21 21:16:05 -0300

    feat: add splash screen

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import Routes from '@/main/routes';
-import {store} from '@/main/store';
+import { store } from '@/main/store';
 import StorybookUIRoot from '@/../storybook';
 import Config from 'react-native-config';
+import { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   return (
     <Provider store={store}>
       <Routes />
